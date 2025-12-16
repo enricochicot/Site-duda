e0ad231e2bd105d9f6f48b78de765009ee322cb5
Enrico Leite <enrico.leite@pca.com.br>
2025-12-15 21:23:23 -0300

    first commit

---
+++ funcionalidades/home.js
@@ -0,0 +1,27 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    if(burger) {
+        burger.addEventListener('click', () => {
+            // Toggle Nav
+            nav.classList.toggle('nav-active');
+
+            // Animate Links
+            navLinks.forEach((link, index) => {
+                if (link.style.animation) {
+                    link.style.animation = '';
+                } else {
+                    link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+                }
+            });
+
+            // Animação do ícone Burger
+            burger.classList.toggle('toggle');
+        });
+    }
+}
+
+// Inicia a função
+navSlide();
